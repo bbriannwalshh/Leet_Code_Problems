ef25ba6e5c8f3a12b668187c4114c3f0b726281f
brian tjoe walsh <bbriannwalshh@gmail.com>
2020-01-27 13:38:30 -0800

    palindromeNumber fixed

--- palindromeNumber.js
+++ palindromeNumber.js
@@ -2,6 +2,8 @@
  * @param {number} x
  * @return {boolean}
  */
+
+
 const isPalindrome = function (x) {
   if (x < 0) return false;
 
@@ -9,15 +11,15 @@ const isPalindrome = function (x) {
 
   while (x >= 10) {
     reversedDigits.push(x % 10);
-    Math.floor(x /= 10);
+    x = Math.floor(x /= 10);
   }
 
+  reversedDigits.push(x);
   let start = 0;
-  let end = reversedDigits.length;
+  let end = reversedDigits.length - 1;
 
   while (start < end) {
-    console.log(start);
-    console.log(end);
+
     if (reversedDigits[start] !== reversedDigits[end]) {
       return false;
     }
@@ -28,36 +30,6 @@ const isPalindrome = function (x) {
 
   return true;
 };
-
-
-
-
-// const isPalindrome = function (x) {
-//   if (x < 0) return false;
-
-//   let reversedDigits = [];
-
-//   while (x >= 10) {
-//     reversedDigits.push(x % 10);
-//     Math.floor(x /= 10);
-//   }
-
-//   let start = 0;
-//   let end = reversedDigits.length;
-
-//   while (start < end) {
-//     console.log(start);
-//     console.log(end);
-//     if (reversedDigits[start] !== reversedDigits[end]) {
-//       return false;
-//     }
-
-//     start += 1;
-//     end -= 1;
-//   }
-
-//   return true;
-// };
 
 
 
