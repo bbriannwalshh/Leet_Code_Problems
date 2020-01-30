5f2d94137b0da54206fbc82e2cacac93717b4466
brian tjoe walsh <bbriannwalshh@gmail.com>
2020-01-29 17:21:22 -0800

    add insertionSort

---
+++ insertionSort.js
@@ -0,0 +1,12 @@
+const insertionSort = function(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currElement = arr[i];
+    for (var j = i - 1; j >= 0 && currElement < arr[j]; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currElement;
+  }
+  return arr;
+};
+
+console.log(insertionSort([4,7,3,2,8,3]));
