c12688cb051b86ce16f0313a39ebdc59cfaf7691
brian tjoe walsh <bbriannwalshh@gmail.com>
2020-01-29 17:06:52 -0800

    add selectionSort

---
+++ selectionSort.js
@@ -0,0 +1,20 @@
+const selectionSort = function(array) {
+
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    let temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+
+  return array;
+};
+
+console.log(selectionSort([9,4,2,5,6,8]));
