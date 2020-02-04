7c8dfdcfc04e6d8b466d7f09cdbbbc5008666476
brian tjoe walsh <bbriannwalshh@gmail.com>
2020-02-04 15:11:48 -0800

    recursive way

--- maxNonAdjacentSum.js
+++ maxNonAdjacentSum.js
@@ -9,20 +9,31 @@
 // maxNonAdjacentSum([2, 7, 9, 3, 4])   // => 15, because 2 + 9 + 4
 // maxNonAdjacentSum([4,2,1,6])         // => 10, because 4 + 6 
 
+// function maxNonAdjacentSum(nums) {
+//   let table = new Array(nums.length).fill(0);
+//   table[0] = nums[0];
+
+//   for (let i = 1; i < nums.length; i++) {
+//     let skipLeftNeighbor = (table[i - 2]) === undefined ? 0 : table[i - 2];
+//     let includeThisNum = skipLeftNeighbor + nums[i];
+//     let notIncludingThisNum = table[i - 1];
+//     table[i] = Math.max(includeThisNum, notIncludingThisNum);
+//   }
+
+//   // console.log(table);
+
+//   return table[table.length -1];
+// }
+
 function maxNonAdjacentSum(nums) {
-  let table = new Array(nums.length).fill(0);
-  table[0] = nums[0];
+  if (nums.length === 0) return 0;
+  
+  let firstEle = nums[0];
 
-  for (let i = 1; i < nums.length; i++) {
-    let skipLeftNeighbor = (table[i - 2]) === undefined ? 0 : table[i - 2];
-    let includeThisNum = skipLeftNeighbor + nums[i];
-    let notIncludingThisNum = table[i - 1];
-    table[i] = Math.max(includeThisNum, notIncludingThisNum);
-  }
-
-  // console.log(table);
-
-  return table[table.length -1];
+  return Math.max(
+    firstEle + maxNonAdjacentSum(nums.slice(2)), 
+    maxNonAdjacentSum(nums.slice(1))
+  );
 }
 
 console.log(maxNonAdjacentSum([2, 7, 9, 3, 4]) );
