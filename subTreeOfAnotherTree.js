d5a4ea1e27e19a3b5d54ec08d870e597f981b3b4
brian tjoe walsh <bbriannwalshh@gmail.com>
2020-02-10 18:57:38 -0800

    add subTreeOfAnotherTree

---
+++ subTreeOfAnotherTree.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+const isSubtree = function (s, t) {
+  let queue = [s];
+
+  while (queue.length) {
+    let tempQueue = [];
+
+    for (let i = 0; i < queue.length; i++) {
+      let curr = queue[i]
+      if (curr.val === t.val && subCheck(curr, t)) return true;
+      if (curr.left) tempQueue.push(curr.left);
+      if (curr.right) tempQueue.push(curr.right);
+    }
+
+    queue = tempQueue;
+  }
+
+  return false;
+};
+
+const subCheck = function (s, t) {
+  let stackS = [s];
+  let stackT = [t];
+
+  while (stackT.length) {
+    let sStackTemp = [];
+    let tStackTemp = [];
+    for (let i = 0; i < stackT.length; i++) {
+      let eleS = stackS[i];
+      let eleT = stackT[i];
+      if (eleS.val !== eleT.val) return false;
+      if (eleS && eleS.left) sStackTemp.push(eleS.left);
+      if (eleS && eleS.right) sStackTemp.push(eleS.right);
+      if (eleT && eleT.left) tStackTemp.push(eleT.left);
+      if (eleT && eleT.right) tStackTemp.push(eleT.right);
+    }
+
+    stackS = sStackTemp;
+    stackT = tStackTemp;
+  }
+
+  return true;
+};
