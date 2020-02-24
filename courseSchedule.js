016a7fe1c34bfa8c5662a3fbf3dcaa26a108f3a0
brian tjoe walsh <bbriannwalshh@gmail.com>
2020-02-23 17:00:28 -0800

    trying to figure out courseSchedule

--- courseSchedule.js
+++ courseSchedule.js
@@ -11,48 +11,64 @@ const sortingFunction = function (a, b) {
   else {
     return (a[0] < b[0]) ? -1 : 1;
   }
-}
+};
 
 
 var canFinish = function (numCourses, prerequisites) {
   if (!prerequisites.length) return true;
-  let sorted = prerequisites.sort(sortingFunction);
   let object = {};
+  let keys = [];
 
-  for (let i = 0; i < sorted.length; i++) {
-    let course = sorted[i];
-    let curr = course[0];
-    let prereq = course[0];
-
-    object[curr] = prereq;
-  }
-
-  for (let i = 0; i < sorted.length; i++) {
-    let course = sorted[i];
+  for (let i = 0; i < prerequisites.length; i++) {
+    let course = prerequisites[i];
     let curr = course[0];
     let prereq = course[1];
 
-    if (dfs(prereq, object, numCourses - 1)) {
+    if (object[curr]) {
+      object[curr].push(prereq);
+    } else {
+      keys.push(curr);
+      object[curr] = [prereq];
+    }
+  }
+
+  for (let i = 0; i < keys.length; i++) {
+    let course = keys[i];
+    let prereqs = object[course];
+    let courses = dfs(prereqs, object);
+
+    if (courses <= numCourses) {
       return true;
     }
   }
+
+  return false;
 };
 
-const dfs = function (prereq, prerequisites, numCourses) {
-  if (numCourses === 0) return false;
-  let currCourses = numCourses - 1;
+const dfs = function (prereqs, prerequisites, visited = {}) {
 
-  let newPreRec = prerequisites[prereq];
-
-  if (!newPreRec) {
-    return true;
+  if (!prereqs || !prereqs.length) {
+    return 1;
   } else {
-    dfs(newPreRec, prerequisites, currCourses);
-  }
+    let counter = 0;
+    for (let i = 0; i < prereqs.length; i++) {
+      if (visited[prereqs[i]]) {
+        return false;
+      } else {
+        visited[prereqs[i]] = true;
+        let answer =  dfs(prereqs[i], prerequisites, visited);
+        if (!answer) {
+          return false;
+        } else {
+          counter += answer;
+        }
+      }
+    }
 
+    return counter;
+  }
 };
 
-
-let numCourses = 2;
-let prerequisites = [[0, 1], [1, 0]];
+let numCourses = 3;
+let prerequisites = [[1, 0], [1, 2], [0, 1]];
 console.log(canFinish(numCourses, prerequisites));
