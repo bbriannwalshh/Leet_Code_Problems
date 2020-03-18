737ba6c78b1a503ebaca5e6c66c71f5745af7c34
brian tjoe walsh <bbriannwalshh@gmail.com>
2020-03-18 15:08:05 -0700

    add smallestOption

--- allOptions.js
+++ allOptions.js
@@ -1,18 +1,50 @@
 
 
 const allOptions = function(arr, target) {
-  if (target === 0) return 1;
-  let numCounter = 0;
+
+  let sorted = arr.sort((a,b) => {
+    if (a > b){
+      return -1;
+    } else {
+      return 1;
+    }
+  });
   
-  for (let i = 0; i < arr.length; i ++) {
-    let curr = arr[i];
+  let options = dfsOptions(sorted, target);
+  
+  return Math.min(...options);
+};
+
+const dfsOptions = function(sorted, target) {
+  if (target === 0) return 0;
+  let options = [];
+
+  for (let i = 0; i < sorted.length; i++) {
+    let curr = sorted[i];
 
     if (curr <= target) {
-      numCounter += allOptions(arr.slice(i), target - curr);
+      options.push(dfsOptions(sorted.slice(i), target - curr));
     }
   }
 
-  return numCounter;
+  let result = [];
+
+  for (let i = 0; i < options.length; i++) {
+    let ele = options[i];
+
+    if (Array.isArray(ele)) {
+      for (let i = 0; i < ele.length; i++) {
+        let subEle = ele[i];
+
+        result.push(subEle + 1);
+      }
+    } else {
+      result.push(ele + 1);
+    }
+
+  }
+
+  return result;
 };
 
 console.log(allOptions([1,2,3], 5));
